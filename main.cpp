3f5dc6e41956433dcc51da0ff781bcb430593a08
nolzaj93 <nolzaj93@gmail.com>
2019-05-15 13:21:50 -0400

    Procedural Project 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
+//Austin Nolz Procedural Project for COP 2001
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    /*
+     * Welcome Message and menu displayed
+     */
+    std::cout << "Welcome to the OraclProduction Ltd Production Line Tracker!\n\n";
+    std::cout << "1. Add a new employee account.\n";
+    std::cout << "2. Add a new music player.\n";
+    std::cout << "3. Add a new movie player.\n";
+    std::cout << "4. Display production statistics.\n";
+    std::cout << "5. Exit.\n";
     return 0;
 }
