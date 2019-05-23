f598d60bdd333eec3c964f0a0bccb46ad63ad3a3
nolzaj93 <nolzaj93@gmail.com>
2019-05-23 14:18:50 -0400

    SBL2 for Procedural Project complete

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #include <iomanip>
 #include <fstream>
 #include <sstream>
-#include "mainPrototypes.h"
+#include "prototypes.h"
 
 /**
  * @brief This is the main function which is the starting point of the program.
@@ -30,7 +30,8 @@ int main() {
     while (programIsRunning) {
 
         //Prompts the user to enter a number between 1 and 5
-        std::cout << "\n" << "Type in a number between 1 and 5 to run the respective function and press enter.\n\n";
+        std::cout << "\n" << "Type in a number between 1 and 5 to run the respective function and press enter.\n"
+                  << std::endl;
 
         showMenu();
 
@@ -54,7 +55,7 @@ int main() {
                 programIsRunning = false;
                 break;
             default:
-                std::cout << "Your input was invalid. Please try again.\n";
+                std::cout << "Your input was invalid. Please try again." << std::endl;
                 break;
         }
     }
@@ -68,7 +69,7 @@ void showMenu() {
     std::cout << "2. Add Employee Account\n";
     std::cout << "3. Add New Product\n";
     std::cout << "4. Display Production Statistics\n";
-    std::cout << "5. Exit\n";
+    std::cout << "5. Exit" << std::endl;
 }
 
 void showCatalog() {
@@ -79,67 +80,13 @@ void showCatalog() {
 
     //If catalog.txt exists then existing products are printed, otherwise it is printed that the catalog is empty.
     if (currentCatalogFile.is_open()) {
-        std::cout << '\n' << "Current catalog: " << '\n';
+        std::cout << '\n' << "Current catalog: " << std::endl;
 
         while (getline(currentCatalogFile, nextLine)) {
-            std::cout << nextLine << '\n';
+            std::cout << nextLine << std::endl;
         }
         currentCatalogFile.close();
-    } else std::cout << "The product catalog is empty.\n";
-}
-
-void addNewProduct() {
-
-    showCatalog();
-
-    std::cout << "\n" << "Enter the Manufacturer" << std::endl;
-    std::string manufacturer;
-    std::cin >> manufacturer;
-
-    //Ignores the newline character read when the user presses enter/return.
-    std::cin.ignore();
-
-    std::string prodName;
-    std::cout << "Enter the Product Name" << std::endl;
-    std::getline(std::cin, prodName);
-
-    std::cout << "Enter the item type\n";
-    std::cout << "1. Audio\n" <<
-              "2. Visual\n" <<
-              "3. AudioMobile\n" <<
-              "4. VisualMobile\n";
-    int itemTypeChoice;
-    std::cin >> itemTypeChoice;
-    std::string itemType;
-
-    // itemType is set depending on itemTypeChoice
-    switch (itemTypeChoice) {
-        case 1:
-            itemType = "Audio";
-            break;
-        case 2:
-            itemType = "Visual";
-            break;
-        case 3:
-            itemType = "AudioMobile";
-            break;
-        case 4:
-            itemType = "VisualMobile";
-            break;
-        default:
-            break;
-    }
-
-    //newProduct string is set by concatenating manufacturer prodName itemType
-    std::string newProduct = manufacturer + " " + prodName + " " + itemType;
-
-    //newProduct string is appended to catalog.txt file
-    std::ofstream catalogFile;
-    catalogFile.open("catalog.txt", std::ios::app);
-    catalogFile << newProduct << std::endl;
-    catalogFile.close();
-
-    std::cout << newProduct << " has been added to the product catalog.\n" << std::endl;
+    } else std::cout << "The product catalog is empty." << std::endl;
 }
 
 void produceItems() {
@@ -158,7 +105,6 @@ void produceItems() {
                     >> visualMobileSerialNum;
         itemNumbers.close();
     } else {
-
         // Production and serial numbers are set to 1 if itemNumbers.txt does not exist.
         productionNumber = 1;
         audioSerialNum = 1;
@@ -170,51 +116,54 @@ void produceItems() {
     showCatalog();
 
     // Eventually the user will be able to choose the item to produce.
-    std::cout << "\n" << "Enter the Manufacturer\n";
     std::string manufacturer;
-    std::cin >> manufacturer;
+    returnConfirmedInput(manufacturer, "manufacturer");
 
-    std::cout << "Enter the Product Name\n";
     std::string prodName;
-    std::getline(std::cin, prodName);
+    returnConfirmedInput(prodName, "product name");
 
-    //Ignores the newline character read when the user presses enter/return.
-    std::cin.ignore();
-
-    std::cout << "Enter the item type\n";
-    std::cout << "1. Audio\n" <<
-              "2. Visual\n" <<
-              "3. AudioMobile\n" <<
-              "4. VisualMobile\n";
-
-    int itemTypeChoice;
-    std::cin >> itemTypeChoice;
+    std::string entryIsCorrect;
+    char itemTypeChoice;
     std::string itemType;
     std::string itemTypeCode;
 
-    // Audio "MM", Visual "VI", AudioMobile "AM", or VisualMobile "VM".
-    switch (itemTypeChoice) {
-        case 1:
-            itemType = "Audio";
-            itemTypeCode = "MM";
-            break;
-        case 2:
-            itemType = "Visual";
-            itemTypeCode = "VI";
-            break;
-        case 3:
-            itemType = "AudioMobile";
-            itemTypeCode = "AM";
-            break;
-        case 4:
-            itemType = "VisualMobile";
-            itemTypeCode = "VM";
-            break;
-        default:
-            break;
-    }
+    do {
+        std::cout << "Please enter the item type\n";
+        std::cout << "1. Audio\n" <<
+                  "2. Visual\n" <<
+                  "3. AudioMobile\n" <<
+                  "4. VisualMobile" << std::endl;
+        std::cin >> itemTypeChoice;
 
-    std::cout << "Enter the number of items that were produced\n";
+        switch (itemTypeChoice) {
+            case '1':
+                itemType = "Audio";
+                itemTypeCode = "MM";
+                break;
+            case '2':
+                itemType = "Visual";
+                itemTypeCode = "VI";
+                break;
+            case '3':
+                itemType = "AudioMobile";
+                itemTypeCode = "AM";
+                break;
+            case '4':
+                itemType = "VisualMobile";
+                itemTypeCode = "VM";
+                break;
+            default:
+                std::cout << "Your input was invalid. Please try again." << std::endl;
+                continue;
+        }
+        //Allows the user to confirm their choice. Loop continues if entryIsCorrect is assigned 0 (false)
+        std::cout << "You entered " + itemType + ". If this is incorrect enter 0 to try again, or enter any other\n"
+                                                 "letter or word to continue." << std::endl;
+        std::cin >> entryIsCorrect;
+
+    } while (entryIsCorrect == "0");
+
+    std::cout << "Please enter the number of items that were produced." << std::endl;
     int numProduced;
     std::cin >> numProduced;
 
@@ -222,18 +171,19 @@ void produceItems() {
     std::ofstream productionFile;
     productionFile.open("production.txt", std::ios::app);
 
-
-    // add a loop to record production, for now simply by
-    // outputting production number and serial number
+    //For loop prints product Info for each product
     for (int counter = 0; counter < numProduced; counter++) {
 
         //productInfo output string stream holds all product information to be written to production.txt
         std::ostringstream productInfo;
-        productInfo << "Manufacturer: " << manufacturer << " Product Name: " << prodName
-                    << " Item Type: " << itemType << " Production Number: " << std::to_string(productionNumber++)
-                    << " Serial Number: " << manufacturer.substr(0, 3) << itemTypeCode;
+        productInfo << "Manufacturer: " << manufacturer << " | Product Name: " << prodName
+                    << " | Item Type: " << itemType << " | Production Number: " << std::to_string(productionNumber++)
+                    << " | Serial Number: " << manufacturer.substr(0, 3) << itemTypeCode;
 
-        //Adds respective serial number to the productInfo ostringstream object
+        /*
+         * Adds respective serial number to the productInfo ostringstream object, serial number is 5 characters wide
+         * filled with zeros and added to the end.
+         */
         if (itemTypeCode == "MM")
             productInfo << std::setfill('0') << std::setw(5) << std::to_string(audioSerialNum++);
         else if (itemTypeCode == "VI")
@@ -254,8 +204,8 @@ void produceItems() {
     itemNumbersOut.open("itemNumbers.txt");
 
     //Writes the production and serial numbers to the itemNumbers.txt file.
-    itemNumbersOut << productionNumber << " " << audioSerialNum << " " << audioMobileSerialNum << " " << visualSerialNum
-                   << " " << visualMobileSerialNum;
+    itemNumbersOut << productionNumber << " " << audioSerialNum << " " << audioMobileSerialNum << " "
+                   << visualSerialNum << " " << visualMobileSerialNum;
     itemNumbersOut.close();
 
 }
@@ -264,18 +214,196 @@ void addEmployeeAccount() {
     std::cout << "Add Employee Account Stub\n";
 }
 
-void addMusicPlayer() {
+void addNewProduct() {
+    //prints the current catalog or let's the user know if it is empty.
+    showCatalog();
 
-    //Adds Audio or AudioMobile player
-    std::cout << "Add Music Player Stub\n";
+    std::string manufacturer;
+
+    returnConfirmedInput(manufacturer, "manufacturer");
+
+    std::string prodName;
+    returnConfirmedInput(prodName, "product name");
+
+    std::cout << "Please enter the item type\n";
+    std::cout << "1. Audio\n" <<
+              "2. Visual\n" <<
+              "3. AudioMobile\n" <<
+              "4. VisualMobile" << std::endl;
+    char itemTypeChoice;
+    std::cin >> itemTypeChoice;
+    std::string itemType;
+
+    std::string productSpecs;
+    // itemType is set depending on itemTypeChoice
+
+    std::string entryIsCorrect;
+    //Sets itemType and productSpecs with a call to either addMusicPlayer or addMoviePlayer. Loop runs until they
+    // confirm the input by entering 1 which sets the bool entryIsCorrect to true.
+    do {
+        switch (itemTypeChoice) {
+            case '1':
+                itemType = "Audio";
+                break;
+            case '2':
+                itemType = "Visual";
+                break;
+            case '3':
+                itemType = "AudioMobile";
+                break;
+            case '4':
+                itemType = "VisualMobile";
+                break;
+            default:
+                std::cout << "Your input was invalid. Please try again." << std::endl;
+                continue;
+        }
+        //Allows the user to confirm their choice. Loop continues if entryIsCorrect is assigned 0 (false)
+        std::cout << "You entered " + itemType + ". If this is incorrect enter 0 to try again, or enter any other\n"
+                                                 "letter or word to continue." << std::endl;
+        std::cin >> entryIsCorrect;
+
+    } while (entryIsCorrect == "0");
+
+    if (itemTypeChoice == '1' || itemTypeChoice == '3')
+        productSpecs = addMusicPlayer();
+    else
+        productSpecs = addMoviePlayer();
+
+    //newProduct string is set by concatenating manufacturer prodName itemType
+    std::string newProduct = manufacturer + " " + prodName + " " + itemType + ", " + productSpecs;
+
+    //newProduct string is appended to catalog.txt file
+    std::ofstream catalogFile;
+    catalogFile.open("catalog.txt", std::ios::app);
+    catalogFile << newProduct << std::endl;
+    catalogFile.close();
+
+    std::cout << newProduct << " has been added to the product catalog.\n" << std::endl;
 }
 
-void addMoviePlayer() {
+std::string addMusicPlayer() {
 
-    //Adds Visual or VisualMobile player
-    std::cout << "Add Movie Player Stub\n";
+    /*
+     * Music Players store AudioSpecification (the file format, like wav or mp3)
+     * and MediaType (what stores the audio file like CD, DVD, Blu-Ray), both input by the user.
+     */
+    std::string entryIsCorrect;
+    std::string audioSpecs;
+
+    //Loop allows the user to enter 0 if they incorrectly typed the formats, or 1 if it is correct.
+    do {
+        //Ignores the newline character read when the user presses enter/return before within addNewProduct().
+        std::cin.ignore();
+
+        std::cout << "Please enter the supported file format(s) for the music player. If there are multiple supported\n"
+                     "formats then separate them with a space. For example, WAV MP3, or WAV FLAC MP3." << std::endl;
+        std::getline(std::cin, audioSpecs);
+
+        std::cout << "You entered " + audioSpecs + ". If this is incorrect enter 0 to try again, or enter any other\n"
+                                                   "letter or word to continue." << std::endl;
+        std::cin >> entryIsCorrect;
+    } while (entryIsCorrect == "0");
+
+    std::string mediaType;
+
+    //Loop runs until the user types 1 to confirm that the entry is correct.
+    do {
+        //Ignores the newline character within the buffer from the previous entry.
+        std::cin.ignore();
+
+        std::cout << "Please enter the media type(s) for the music player. For example, Digital, CD, DVD, or Blu-Ray."
+                  << std::endl;
+        std::getline(std::cin, mediaType);
+
+        std::cout << "You entered " + mediaType + ". If this is incorrect enter 0 to try again, or enter any other\n"
+                                                  "letter or word to continue." << std::endl;
+        std::cin >> entryIsCorrect;
+    } while (entryIsCorrect == "0");
+
+    std::string musicPlayerSpecs = "File format(s): " + audioSpecs + ", " + "Media Type(s): " + mediaType;
+
+    return musicPlayerSpecs;
+}
+
+std::string addMoviePlayer() {
+
+    /*
+     * Movie Players store MonitorType, which is a Screen.
+     * Screen stores: resolution, refresh rate, response time.
+     */
+    std::string monitorType;
+    char screen;
+    std::string entryIsCorrect;
+
+    //Loop runs until valid input is entered and the user enters 1 to confirm the correct entry.
+    do {
+        std::cout << "Please enter the number before the type of screen for this product.\n"
+                     "1. LCD \n"
+                     "2. LED \n"
+                     "3. OLED." << std::endl;
+        std::cin >> screen;
+
+        switch (screen) {
+            case '1':
+                monitorType = "LCD";
+                break;
+            case '2':
+                monitorType = "LED";
+                break;
+            case '3':
+                monitorType = "OLED";
+                break;
+            default:
+                std::cout << "Your input is invalid. Please try again.\n";
+                continue;
+        }
+        //Confirms the user's entry.
+        std::cout << "You chose " + monitorType + ". If this is incorrect enter 0 to try again, or enter any other\n"
+                                                  "letter or word to continue." << std::endl;
+        std::cin >> entryIsCorrect;
+
+    } while (entryIsCorrect == "0");
+
+    //stores the screen's resolution, refresh rate, response time.
+    std::string screenSpecs;
+
+    //Loop runs until the user types 1 to confirm that the entry is correct.
+    do {
+        //Ignores the newline character read when the user presses enter/return previously.
+        std::cin.ignore();
+
+        std::cout << "Please enter the screen's resolution, refresh rate, and response time. For example,"
+                     " 1080p 144 Hz 1 ms." << std::endl;
+        std::getline(std::cin, screenSpecs);
+
+        std::cout << "You entered " + screenSpecs + ". If this is incorrect enter 0 to try again, or enter any other\n"
+                                                    "letter or word to continue." << std::endl;
+        std::cin >> entryIsCorrect;
+    } while (entryIsCorrect == "0");
+
+    std::string moviePlayerSpecs = "Monitor Type: " + monitorType + ", " + "Screen Specifications: " + screenSpecs;
+
+    return moviePlayerSpecs;
 }
 
 void displayProductionStatistics() {
     std::cout << "Display Production Statistics Stub\n";
+}
+
+void returnConfirmedInput(std::string &variable, const std::string variableName) {
+    std::string entryIsCorrect;
+    do {
+        //Ignores the newline character read when the user presses enter/return.
+        std::cin.ignore();
+
+        std::cout << "\n" << "Please enter the " + variableName + "." << std::endl;
+        std::getline(std::cin, variable);
+
+        //Allows the user to confirm their choice. Loop continues if entryIsCorrect is assigned 0 (false)
+        std::cout << "You entered " + variable + ". If this is incorrect enter 0 to try again, or enter any other\n"
+                                                 "letter or word to continue." << std::endl;
+        std::cin >> entryIsCorrect;
+
+    } while (entryIsCorrect == "0");
 }
---
+++ prototypes.h
@@ -0,0 +1,62 @@
+/** @file prototypes.h
+ *  @brief This file is the header file which defines the function prototypes for the COP2001 Procedural Project.
+ *
+ *  @author Austin Nolz
+ *  @bug - No known bugs currently.
+ */
+
+#ifndef COP2001PROCEDURALPROJECT_MAINPROTOTYPES_H
+#define COP2001PROCEDURALPROJECT_MAINPROTOTYPES_H
+
+#endif //COP2001PROCEDURALPROJECT_MAINPROTOTYPES_H
+
+/**
+ * @brief Prints the menu to the console.
+ */
+void showMenu();
+
+/**
+ * @brief Prints the catalog to the console, and prints that the catalog is empty if no products have been added.
+ */
+void showCatalog();
+
+/**
+ * @brief Allows the user to track the production of each product within the catalog by writing to the production.txt
+ *        file.
+ */
+void produceItems();
+
+/**
+ * @brief Allows an administrator to add an employee account.
+ */
+void addEmployeeAccount();
+
+/**
+ * @brief Allows the user to add a new product to the catalog by writing to the catalog.txt file.
+ */
+void addNewProduct();
+
+/**
+ * @brief This function adds a music player to the catalog.
+ * @return Sends the Music Player specifications to the addNewProduct function to be added to productInfo.
+ */
+std::string addMusicPlayer();
+
+/**
+ * @brief This function adds a movie player to the catalog.
+ * @return Sends the Movie Player specifications to the addNewProduct function to be added to productInfo.
+ */
+
+std::string addMoviePlayer();
+
+/**
+ * @brief Displays productions statistics to the user.
+ */
+void displayProductionStatistics();
+
+/**
+ * @brief Takes the reference variable as 1st argument, variable name as 2nd argument, and confirms the user's input.
+ * @param 1st parameter is named variable, which is a reference variable to the corresponding required input
+ * @param 2nd parameter is named varName, which is a string literal referring to the variable's name.
+ */
+void returnConfirmedInput(std::string&, const std::string);
